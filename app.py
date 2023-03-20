7aed01c307bf474caee8c4fe73c6d91fa2b5bf6f
brennanp93 <brennan.p.perez@gmail.com>
2023-03-20 12:56:30 -0700

    update formatting on app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=("GET","POST"))
@@ -13,8 +14,9 @@ def index():
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(movie_description),
-            temperature=0.6,            
+            temperature=0.3,            
         )
+        
         return redirect(url_for("index", result=response.choices[0].text))
     result = request.args.get("result")
     return render_template("index.html", result=result)
