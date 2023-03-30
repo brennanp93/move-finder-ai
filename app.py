beff741ff00434c68e68fd01d5291394c2a9e6dd
brennanp93 <brennan.p.perez@gmail.com>
2023-03-29 18:24:29 -0700

    add twitter share button

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(description),
-            temperature=0.3,            
+            temperature=0.7,            
         )
         # session['input'] = description
        
@@ -22,17 +22,30 @@ def index():
         return redirect(url_for("index", result=response.choices[0].text, input=description))
     result = request.args.get("result")
     input = request.args.get("input")
+    session['user_input'] = input
+    session['output'] = result
     return render_template("index.html", result=result, input=input )
 
+
+@app.route('/share/twitter')
+def share_twitter():
+    user_input=session['user_input']
+    output=session['output']
+    url='https://movie-finder-ai.herokuapp.com/'
+    text=f'Wow! I input:\n"{user_input}"\nAnd it correctly guessed:\n"{output}"\nSee if you can stump it!'
+    tweet_url=f'https://twitter.com/intent/tweet?url={url}&text={text}'
+    return redirect(tweet_url)
 
 def generate_prompt(movie_description):
     return """Help me find out the name of a movie based on a brief description. 
 
-Description: Movie where Leonardo Dicaprio plays a con artist
-Movie: Catch Me If You Can. Released in 2002
+Description: Movie where Leonardo Dicaprio plays a con artist.
+Movie: Catch Me If You Can. Released in 2002.
 Description: Movie where Simon Pegg fights zombies with his friend.
-Movie: Shaun of the Dead. Realsed in 2004
-Description: Movie where a weird guy takes over a tv station in the 80's
-Movie: UHF. Released in 1989
+Movie: Shaun of the Dead. Realsed in 2004.
+Description: Movie where a weird guy takes over a tv station in the 80's.
+Movie: UHF. Released in 1989.
+Description: Jean reno and Jon Voight are bad guys.
+Movie: Mission: Impossible. Released in 1996.
 Description: {}
 Movie:""".format(movie_description.capitalize())
